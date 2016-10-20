abc5680f8fb1ef37e3e1ad0808dae8245774d195
Vinod Mohanan <O-OF-N@users.noreply.github.com>
2016-10-19 20:51:52 -0400

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Reduce {
 	private static List<Integer> running = new LinkedList<>();
-	private LinkedHashSet<LinkedHashSet<Integer>> master = new LinkedHashSet<>();
+	private LinkedList<LinkedHashSet<Integer>> master = new LinkedList<>();
 
 	public static void main(String[] args) {
 		running.addAll(Arrays.asList(new Integer[] { 15, 36, 27, 15 }));
@@ -16,12 +16,12 @@ public class Reduce {
 		System.out.println(r1.callAppend());
 	}
 
-	public LinkedHashSet<LinkedHashSet<Integer>> callAppend() {
+	public LinkedList<LinkedHashSet<Integer>> callAppend() {
 		return append(null);
 	}
 
-	public LinkedHashSet<LinkedHashSet<Integer>> append(LinkedHashSet<LinkedHashSet<Integer>> set) {
-		LinkedHashSet<LinkedHashSet<Integer>> s1 = new LinkedHashSet<>();
+	public LinkedList<LinkedHashSet<Integer>> append(LinkedList<LinkedHashSet<Integer>> set) {
+		LinkedList<LinkedHashSet<Integer>> s1 = new LinkedList<>();
 		running.forEach(a1 -> {
 			if (set != null && set.size() > 0)
 				set.forEach(a2 -> addToSet(s1, a1, a2));
@@ -35,7 +35,7 @@ public class Reduce {
 		return master;
 	}
 
-	public void addToSet(LinkedHashSet<LinkedHashSet<Integer>> s1, Integer a1, LinkedHashSet<Integer> a2) {
+	public void addToSet(LinkedList<LinkedHashSet<Integer>> s1, Integer a1, LinkedHashSet<Integer> a2) {
 		LinkedHashSet<Integer> a3 = new LinkedHashSet<>();
 		a3.addAll(a2);
 		List<Integer> a2List = new ArrayList<>(a2);
